9e5edcab8f25cc0f078ffc3e9c76f31be022a7ff
lai-luv <l.lavaughn319@gmail.com>
2024-04-25 14:35:41 -0400

    completion of assingment

--- Assignment.js
+++ Assignment.js
@@ -7,9 +7,10 @@
     // Build a Login component that accepts the users email and password.
     
 // Pseudocode:
-    // 
-    // 
-    // 
+    // Display a form with input fileds for email or username and password 
+    // collect user info 
+    // validate user info 
+    // create turnariy for error 
 
 
 
@@ -21,9 +22,9 @@
     // Build a side menu component that has 5 buttons.
 
 // Pseudocode:
-    // 
-    // 
-    // 
+    // display a side menue with 5 button
+    // Each button represents a different action or page.
+    // Handle button clicks to perform the corresponding action or navigate to the respective page.
 
 
 
@@ -35,9 +36,9 @@
     // Build a filter component that can filter chart data.
 
 // Pseudocode:
-    // 
-    // 
-    // 
+    //1. Display filter options based on the data to be filtered
+    //2.Capture user selections or input.
+    //3.Update the chart based on the applied filters.
 
 
 
@@ -49,10 +50,14 @@
     // Build a profile component where users can add and edit their profile information.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
+    // 1. Display user profile information such as name, email, bio, etc.
+    // 2. Provide options to edit each field of the profile.
+    // 3. Capture user input for editing profile information.
+    // 4. Validate the edited information.
+    // 5. If profile update is successful, display a success message to the user.
+    // 6. If profile update fails, display an error message to the user.
+
+     
 
 
 
@@ -63,7 +68,10 @@
     // Build a bar chart component that displays 2 sets of data from the backend.
 
 // Pseudocode:
-    // 
-    // 
-    // 
-    // 
+    // 1. Fetch data from the backend required for the bar chart.
+    // 2. Prepare the data for visualization .
+    // 3. Display a bar chart with the fetched data.
+    // 4. Customize the chart to display the two sets of data distinctively 
+    // 5. Update the chart dynamically if there are changes in the data.
+
+   
